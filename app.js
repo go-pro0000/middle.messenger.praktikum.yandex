23dd01078c29ed0a8c0cec7cbed3545c1b08a9c4
go-pro0000 <igor.pronin.2000@acgnn.ru>
2023-04-13 05:41:28 +0300

    CSP

--- app.js
+++ app.js
@@ -3,6 +3,13 @@ const PORT = process.env.PORT || 3000;
  
 const app = express();
 app.use(express.static(__dirname));
+app.use(function (req, res, next) {
+  res.setHeader(
+    'Content-Security-Policy',
+    "default-src 'self'; font-src 'self'; img-src 'self'; script-src 'self'; style-src 'self'; frame-src 'self'"
+  );
+  next();
+});
 
 app.get('/', function(req, res) {
   res.sendFile(__dirname + "/dist/index.html")
